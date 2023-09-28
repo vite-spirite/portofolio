28d583a48cfd5f58fd5581f99bb368728f847a1e
Mathéo Boulanger <boulangermatheo62@gmail.com>
2023-09-28 17:13:42 +0200

    add SEO balises

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()],
+  site: 'https://matheoboulanger.netlify.app/',
+  output: "server",
+  adapter: netlify()
 });
