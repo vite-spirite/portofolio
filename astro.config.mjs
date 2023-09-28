1247d0a9d52ad583ca31c3e64beba940d6365c93
Mathéo Boulanger <boulangermatheo62@gmail.com>
2023-09-28 16:13:33 +0200

    fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'vite-spirite.github.io',
+  site: 'https://vite-spirite.github.io',
   base: '/protofolio'
 });
